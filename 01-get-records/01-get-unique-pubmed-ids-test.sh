cbe2b24dfbf2c3d3a88e476e57dcca1f8c146122
Cody Collier <cody@telnet.org>
2017-04-29 22:32:28 -0500

    unique id extraction for the test topics

--- 01-get-records/01-get-unique-pubmed-ids-test.sh
+++ 01-get-records/01-get-unique-pubmed-ids-test.sh
@@ -2,12 +2,16 @@
 #
 # . get the unique set of pubmed ids from the dev/test set
 #
+# sed help:
+# http://stackoverflow.com/questions/7103531/how-to-get-the-part-of-file-after-the-line-that-matches-grep-expression-first
+#
+#
 
 cd $(dirname $0)
 mkdir -p ../downloads/pubmed-docs-test/
 
-if [ ! -f ./list-of-pubmed-ids-dev ]; then
-    cat ../downloads/Training\ Data/qrel_* | tr -s ' ' | cut -f3 -d" " | sort | uniq > list-of-pubmed-ids-test
+if [ ! -f ./list-of-pubmed-ids-test ]; then
+    sed -e '1,/Pids:/d' ../downloads/topics_test/* | tr -s ' ' | cut -f2 -d" " | sort | uniq > list-of-pubmed-ids-test
 fi
 ls -la list-of-pubmed-ids-test
 
